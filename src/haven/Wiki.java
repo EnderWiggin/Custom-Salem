00196f20ce859575fa9c79426b90dc76b7666579
EnderWiggin <andrii.gook@gmail.com>
2012-08-17 13:41:10 +0300

    changed wiki property names to new ones and disabled recursive and backlinking in RDF export

--- src/haven/Wiki.java
+++ src/haven/Wiki.java
@@ -21,21 +21,21 @@ public class Wiki {
     static private Map<String, Map<String, Integer>> DB = new HashMap<String, Map<String,Integer>>();
     
     static {
-	pmap.put("property:Arts", "arts");
-	pmap.put("property:Cloak", "cloak");
-	pmap.put("property:Faith", "faith");
-	pmap.put("property:Frontier", "wild");
-	pmap.put("property:Hammer", "nail");
-	pmap.put("property:Hunting", "hung");
-	pmap.put("property:Law", "law");
-	pmap.put("property:Mines", "mine");
-	pmap.put("property:Pots", "pots");
-	pmap.put("property:Sparks", "fire");
-	pmap.put("property:Stocks", "stock");
-	pmap.put("property:Sugar", "spice");
-	pmap.put("property:Thread", "thread");
-	pmap.put("property:Natural", "natp");
-	pmap.put("property:Perennial", "perp");
+	pmap.put("property:Gives_Arts_-26_Crafts", "arts");
+	pmap.put("property:Gives_Cloak_-26_Dagger", "cloak");
+	pmap.put("property:Gives_Faith_-26_Wisdom", "faith");
+	pmap.put("property:Gives_Frontier_-26_Wilderness", "wild");
+	pmap.put("property:Gives_Hammer_-26_Nail", "nail");
+	pmap.put("property:Gives_Hunting_-26_Gathering", "hung");
+	pmap.put("property:Gives_Law_-26_Lore", "law");
+	pmap.put("property:Gives_Mines_-26_Mountains", "mine");
+	pmap.put("property:Gives_Pots_-26_Pans", "pots");
+	pmap.put("property:Gives_Sparks_-26_Embers", "fire");
+	pmap.put("property:Gives_Stocks_-26_Cultivars", "stock");
+	pmap.put("property:Gives_Sugar_-26_Spice", "spice");
+	pmap.put("property:Gives_Thread_-26_Needle", "thread");
+	pmap.put("property:Gives_Natural_Philosophy", "natp");
+	pmap.put("property:Gives_Perennial_Philosophy", "perp");
 
 	Thread t = new HackThread(new Runnable() {
 	    
@@ -75,7 +75,7 @@ public class Wiki {
 
     private static void load(String name){
 	try {
-	    String url = String.format("http://salemwiki.info/index.php/Special:ExportRDF/%s", name);
+	    String url = String.format("http://salemwiki.info/index.php/Special:ExportRDF/%s?recursive=0&backlinks=0", name);
 	    //parse
 	    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 	    DocumentBuilder builder = factory.newDocumentBuilder();
