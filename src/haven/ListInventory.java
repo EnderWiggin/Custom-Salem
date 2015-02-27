b7ec7b3785ff174694835ea56d7998683f59da6c
EnderWiggin <andrii.gook@gmail.com>
2015-02-27 17:50:30 +0200

    first test of stacked items

--- src/haven/ListInventory.java
+++ src/haven/ListInventory.java
@@ -1,23 +1,24 @@
 package haven;
 
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
 public class ListInventory extends Widget {
     static Tex item_bg = ISBox.bg;
     static Coord item_sz = item_bg.sz();
-    Listbox<WItem> list;
+    Listbox<StackedItem> list;
     Inventory inv;
     long changed = 0;
-    List<WItem> items;
+    List<StackedItem> items;
 
     public ListInventory(Coord c, Coord sz, Inventory inventory) {
 	super(c, sz, inventory.parent);
 	inv = inventory;
 
-	list = new Listbox<WItem>(Coord.z, this, item_sz.x+10, 5, item_sz.y) {
+	list = new Listbox<StackedItem>(Coord.z, this, item_sz.x+10, 5, item_sz.y) {
 	    @Override
-	    protected WItem listitem(int i) {
+	    protected StackedItem listitem(int i) {
 		return items.get(i);
 	    }
 
@@ -27,10 +28,12 @@ public class ListInventory extends Widget {
 	    }
 
 	    @Override
-	    protected void drawitem(GOut g, WItem item) {
-		g.image(item_bg, Coord.z);
-		item.draw(g);
-		g.text(item.item.name(), new Coord(45, 0));
+	    protected void drawitem(GOut g, StackedItem item) {
+		try {
+		    g.image(item_bg, Coord.z);
+		    item.draw(g);
+		    g.text(item.name(), new Coord(45, 0));
+		}catch (Resource.Loading ignored){}
 	    }
 	};
 	list.bgcolor = null;
@@ -47,15 +50,59 @@ public class ListInventory extends Widget {
 
     @Override
     public Object tooltip(Coord c, Widget prev) {
-	WItem item = list.itemat(c.sub(list.c));
+	StackedItem item = list.itemat(c.sub(list.c));
 	if(item != null) {
-	    return item.tooltip(Coord.z, (prev==this)?item:prev);
+	    //return item.tooltip(Coord.z, (prev==this)?item:prev);
 	}
 	return null;
     }
 
     private void invchanged() {
-	items = new LinkedList<WItem>(inv.wmap.values());
+	Collection<WItem> values = inv.wmap.values();
+	items = new LinkedList<StackedItem>();
+	for(WItem item : values){
+	    boolean found = false;
+	    for(StackedItem sitem : items){
+		if(sitem.put(item)){
+		    found = true;
+		    break;
+		}
+	    }
+	    if(!found){
+		items.add(new StackedItem(item));
+	    }
+
+	}
+    }
+
+    public static class StackedItem {
+	public List<WItem> list;
+	public StackedItem(WItem item) {
+	    list = new LinkedList<WItem>();
+	    list.add(item);
+	}
+
+	public boolean put(WItem item){
+	    if(list.get(0).item.name().equals(item.item.name())){
+		list.add(item);
+		return true;
+	    }
+	    return false;
+	}
+
+	public String name(){
+	    return String.format("%s x%d", list.get(0).item.name(), list.size());
+	}
+
+	public void draw(GOut g){
+	    try {
+		list.get(0).draw(g);
+	    }catch (Resource.Loading ignored){}
+	}
+
+	public Object tooltip(Coord c, Widget prev){
+	    return list.get(0).tooltip(c, prev);
+	}
     }
 
 }
