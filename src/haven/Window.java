b156db6655508fca7bba2a622ed949dcfff854b9
Fredrik Tolf <fredrik@dolda2000.com>
2015-03-05 05:32:23 +0100

    Fixed window focus issue.

--- src/haven/Window.java
+++ src/haven/Window.java
@@ -180,12 +180,12 @@ public class Window extends Widget implements DTarget {
     }
 
     public boolean mousedown(Coord c, int button) {
+	if(c.y < tdh)
+	    return(false);
 	parent.setfocus(this);
 	raise();
 	if(super.mousedown(c, button))
 	    return(true);
-	if(c.y < tdh)
-	    return(false);
 	if(button == 1) {
 	    ui.grabmouse(this);
 	    dm = true;
