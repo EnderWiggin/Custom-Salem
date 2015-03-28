918bddf5e1258c7b8745c013fe784126c75c3018
johncarver <johncarver@salemserver.home>
2015-03-28 11:37:30 -0700

    Kin names stop disappearing.

--- src/haven/KinInfo.java
+++ src/haven/KinInfo.java
@@ -101,8 +101,8 @@ public class KinInfo extends GAttrib {
 			int tm = (int)(now - seen);
 			Color show = null;
 			boolean auto = (type & 1) == 0;
-			if(auto && (tm < 7500)) {
-			    show = Utils.clipcol(255, 255, 255, 255 - ((255 * tm) / 7500));
+			if(auto) {
+			    show = Color.WHITE;
 			}
 			if(show != null) {
 			    Tex t = rendered();
