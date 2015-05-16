4f8f2092f983406c34dca2aa405cf096536aae94
EnderWiggin <andrii.gook@gmail.com>
2015-05-16 11:13:40 +0300

    added displaying of server date in season widget tooltip

--- src/haven/SeasonImg.java
+++ src/haven/SeasonImg.java
@@ -27,8 +27,12 @@
 package haven;
 
 import java.awt.Color;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class SeasonImg extends Widget {
+    private static final SimpleDateFormat datef = new SimpleDateFormat("yyyy-MM-dd, HH:mm:ss", Locale.ENGLISH);
+    private static final long EPOCH;
     private static final Tex seasons[] = {
 	    Resource.loadtex("gfx/hud/coldsnap"),
 	    Resource.loadtex("gfx/hud/everbloom"),
@@ -37,11 +41,20 @@ public class SeasonImg extends Widget {
     public static final IBox box = Window.swbox;
     public static final Color color = new Color(133, 92, 62);
     private final Coord isz, ic;
+    private long time = 0;
+    private Tex timeTex = null;
+
+    static {
+	Calendar c = new GregorianCalendar(1631, 0, 1);//Year when Providence was established
+	c.setTimeZone(TimeZone.getTimeZone("GMT"));
+	EPOCH = c.getTimeInMillis();
+    }
 
     public SeasonImg(Coord c, Coord sz, Widget parent) {
 	super(c, sz, parent);
 	isz = sz.sub(box.bisz());
 	ic = box.btloff();
+	datef.setTimeZone(TimeZone.getTimeZone("GMT"));
     }
     
     public void draw(GOut g) {
@@ -50,5 +63,20 @@ public class SeasonImg extends Widget {
 	g.chcolor(color);
 	box.draw(g, Coord.z, sz);
 	g.chcolor();
+    }
+
+    @Override
+    public Object tooltip(Coord c, Widget prev) {
+	long stime = ui.sess.glob.globtime();
+	if ((int) (time / 1000) != (int) (stime / 1000) || timeTex == null) {
+	    time = stime;
+	    if (timeTex != null) {
+		timeTex.dispose();
+	    }
+	    Date date = new Date(time + EPOCH);
+	    timeTex = Text.render(datef.format(date)).tex();
+	}
+
+	return timeTex;
     }
 }
