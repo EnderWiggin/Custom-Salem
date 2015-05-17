aad83e361a18c0b8cd50b3666533c8a3ed8db320
EnderWiggin <andrii.gook@gmail.com>
2015-05-17 15:27:46 +0300

    fixed R2DWdg z-order

--- src/haven/R2DWdg.java
+++ src/haven/R2DWdg.java
@@ -7,14 +7,13 @@ public class R2DWdg extends Widget {
     public R2DWdg(PView target) {
 	super(target.c, target.sz, target.parent);
 	this.target = target;
+	lower();
     }
 
     @Override
     public void draw(GOut g) {
 	super.draw(g);
 	g.chcolor();
-	Coord s1 = ui.root.sz;
-	Coord s2 = ui.gui.sz;
 	if(target.rls == null){return;}
 	for(RenderList.Slot s : target.rls.slots()) {
 	    if(s.r instanceof PView.Render2D)
