bed3d58a472ed2df5edd9a1e5ff2012f9e2db71c
EnderWiggin <andrii.gook@gmail.com>
2015-05-21 22:02:55 +0300

    fixed tabulation

--- src/org/ender/timer/Timer.java
+++ src/org/ender/timer/Timer.java
@@ -11,7 +11,7 @@ public class Timer {
 	this.duration = duration;
     }
 
-    public interface  Callback {
+    public interface Callback {
 	public void run(Timer timer);
     }
 
@@ -26,32 +26,33 @@ public class Timer {
     transient private long remaining;
     transient public Callback updater;
 
-    public Timer(){}
+    public Timer() {
+    }
 
-    public boolean isWorking(){
+    public boolean isWorking() {
 	return start != 0;
     }
     
-    public void stop(){
+    public void stop() {
 	start = 0;
-	if(updater != null){
+	if (updater != null) {
 	    updater.run(this);
 	}
 	TimerController.getInstance().save();
     }
     
-    public void start(){
-	start = server + SERVER_RATIO*(System.currentTimeMillis() - local);
+    public void start() {
+	start = server + SERVER_RATIO * (System.currentTimeMillis() - local);
 	TimerController.getInstance().save();
     }
     
-    public synchronized boolean update(){
+    public synchronized boolean update() {
 	long now = System.currentTimeMillis();
-	remaining = (duration - now + local - (server - start)/SERVER_RATIO);
-	if(remaining <= 0){
-	    Window wnd = new Window(new Coord(250,100), Coord.z, UI.instance.root, name);
+	remaining = (duration - now + local - (server - start) / SERVER_RATIO);
+	if (remaining <= 0) {
+	    Window wnd = new Window(new Coord(250, 100), Coord.z, UI.instance.root, name);
 	    String str;
-	    if(remaining < -1500){
+	    if (remaining < -1500) {
 		str = String.format("%s elapsed since timer named \"%s\"  finished it's work", toString(), name);
 	    } else {
 		str = String.format("Timer named \"%s\" just finished it's work", name);
@@ -61,43 +62,42 @@ public class Timer {
 	    wnd.pack();
 	    return true;
 	}
-	if(updater != null){
+	if (updater != null) {
 	    updater.run(this);
 	}
 	return false;
     }
     
     public synchronized long getStart() {
-        return start;
+	return start;
     }
 
     public synchronized void setStart(long start) {
-        this.start = start;
+	this.start = start;
     }
 
     public synchronized String getName() {
-        return name;
+	return name;
     }
 
     public synchronized void setName(String name) {
-        this.name = name;
+	this.name = name;
     }
 
-    public synchronized long getDuration()
-    {
+    public synchronized long getDuration() {
 	return duration;
     }
 
     @Override
     public String toString() {
-	long t = Math.abs(isWorking()? remaining : duration)/1000;
-	int h = (int) (t/3600);
-	int m = (int) ((t%3600)/60);
-	int s = (int) (t%60);
-	return String.format("%d:%02d:%02d", h,m,s);
+	long t = Math.abs(isWorking() ? remaining : duration) / 1000;
+	int h = (int) (t / 3600);
+	int m = (int) ((t % 3600) / 60);
+	int s = (int) (t % 60);
+	return String.format("%d:%02d:%02d", h, m, s);
     }
     
-    public void destroy(){
+    public void destroy() {
 	TimerController.getInstance().remove(this);
 	updater = null;
     }
