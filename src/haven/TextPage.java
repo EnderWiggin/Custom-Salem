16c6f0e928c5ee586cb506370800dc1b8d2e1a94
EnderWiggin <andrii.gook@gmail.com>
2015-05-26 17:50:16 +0300

    added widgets for text page displaying and writing

---
+++ src/haven/TextPage.java
@@ -0,0 +1,44 @@
+package haven;
+
+import java.awt.*;
+import java.awt.font.TextAttribute;
+
+public class TextPage extends RichTextBox {
+    private static final RichText.Foundry fnd;
+    private final Tex paper = Resource.loadtex("gfx/hud/blankpaper");
+
+    static {
+	fnd = new RichText.Foundry(
+	    TextAttribute.FAMILY, 	"Serif",
+	    TextAttribute.SIZE,		18,
+	    TextAttribute.FOREGROUND,	new Color(0x22053D)//Color.BLACK
+	);
+	fnd.aa = true;
+    }
+
+    @RName("textpage")
+    public static class $_ implements Factory {
+	public Widget create(Coord c, Widget parent, Object[] args) {
+	    return new TextPage(c, (Coord) args[0], parent, (String) args[1]);
+	}
+    }
+
+    public TextPage(Coord c, Coord sz, Widget parent, String text) {
+	super(c, sz, parent, "", fnd);
+	bg = null;
+	settext(text);
+    }
+
+    @Override
+    public void settext(String text) {
+	text = text.replaceAll("/n", "\n");
+	text = text.replaceAll("\\$t\\{", "\\$size[30]{");
+	super.settext(text);
+    }
+
+    @Override
+    public void draw(GOut g) {
+	g.image(paper, Coord.z);
+	super.draw(g);
+    }
+}
